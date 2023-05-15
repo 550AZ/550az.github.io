7b7e4c79bd9fe9d69bd47f280f5f0ce9b2f132a4
AlbertZhou <125489857+hiker18@users.noreply.github.com>
2023-05-16 00:32:02 +0800

    Update Geometry.py

--- Programs/Python/Geometry.py
+++ Programs/Python/Geometry.py
@@ -4,39 +4,117 @@
 
 import turtle
 
-# Create a turtle named tom
-tom = turtle.Turtle()
+# Create a turtle named t
+t = turtle.Turtle()
 
 # Draw a circle
-tom.circle(50)
+t.circle(50)
 
 # Move to a new location
-tom.penup()
-tom.goto(100, 100)
-tom.pendown()
+t.penup()
+t.goto(100, 100)
+t.pendown()
 
 # Draw a rectangle
 for i in range(2):
-    tom.forward(100)
-    tom.left(90)
-    tom.forward(50)
-    tom.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(50)
+    t.left(90)
 
 # Move to a new location
-tom.penup()
-tom.goto(-100, 100)
-tom.pendown()
+t.penup()
+t.goto(-100, 100)
+t.pendown()
 
 # Draw a hexagon
 for i in range(6):
-    tom.forward(50)
-    tom.left(60)
+    t.forward(50)
+    t.left(60)
+
+# 移动画笔
+t.penup()
+t.goto(100, 300)
+t.pendown()
+
+# 画“2”
+t.color("black")
+t.right(90)
+t.forward(50)
+t.right(90)
+t.forward(50)
+t.right(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+
+# 移动画笔
+t.penup()
+t.goto(160, 300)
+t.pendown()
+
+# 画“0”
+t.color("black")
+t.forward(50)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(50)
+t.right(90)
+t.forward(100)
+
+# 移动画笔
+t.penup()
+t.goto(220, 300)
+t.pendown()
+
+# 画“2”
+t.color("black")
+t.right(90)
+t.forward(50)
+t.right(90)
+t.forward(50)
+t.right(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+
+# 移动画笔
+t.penup()
+t.goto(280, 300)
+t.pendown()
+
+# 画“1”
+t.color("black")
+t.right(90)
+t.forward(100)
+
+#移动画笔
+t.penup()
+t.goto(340, 250)
+t.pendown()
+
+# 画“8”
+for i in range(2):
+  t.forward(50)
+  t.right(90)
+  t.forward(50)
+  t.right(90)
+
+for i in range(2):
+  t.forward(-50)
+  t.right(-90)
+  t.forward(-50)
+  t.right(-90)
+
+t.hideturtle()  # 隐藏画笔
 
 # Wait for the user to close the window
 turtle.exitonclick()
 
-# Save the image to a file
-# turtle.getscreen().getcanvas().postscript(file="geometry.eps")
 
-# Save the image to a file
-# turtle.getscreen().getcanvas().postscript(file="geometry.eps")
+turtle.done()   # 释放画笔
